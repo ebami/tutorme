fcfd98c28db6d47a12a6b102cb8c9a5d5473c5ea
Emmanuel <ebamigbade@yahoo.co.uk>
2020-05-19 22:08:49 +0100

    Add video to page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import '../Scss/_HomePage.css';
+import React from "react";
+import "../Scss/_HomePage.css";
+import Hls from "hls.js";
 
-export default function HomePage() {
-    return (
-    <div></div>
-    );    
+class HomePage extends React.Component {
+    componentDidMount() {
+        if (Hls.isSupported()) {
+            var video = this.player;
+            var hls = new Hls();
+            // bind them together
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+              console.log("video and hls.js are now bound together !");
+              hls.loadSource("http://d3rpmkia6uhh0i.cloudfront.net/f9f8ac9a-2220-40b3-b318-f9633f996684/hls/20200518_214550.m3u8");
+              hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+                console.log("manifest loaded, found " + data.levels.length + " quality level");
+              });
+            });
+          }
+      }
+      render() {
+        return (
+            <div>
+                <p>Video 1</p>
+              <video id="video"
+                preload="none"
+                className="videoCanvas"
+                ref={(player) => (this.player = player)}
+                controls
+                autoPlay={false}
+              />
+            </div>
+          );
+      }
 }
+export default HomePage
