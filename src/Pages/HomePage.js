92295adf2d2e5c681266b37dcf2990ba4c4aad2b
Emmanuel <ebamigbade@yahoo.co.uk>
2020-05-19 22:16:43 +0100

    change to https

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -11,7 +11,7 @@ class HomePage extends React.Component {
             hls.attachMedia(video);
             hls.on(Hls.Events.MEDIA_ATTACHED, function () {
               console.log("video and hls.js are now bound together !");
-              hls.loadSource("http://d3rpmkia6uhh0i.cloudfront.net/f9f8ac9a-2220-40b3-b318-f9633f996684/hls/20200518_214550.m3u8");
+              hls.loadSource("https://d3rpmkia6uhh0i.cloudfront.net/f9f8ac9a-2220-40b3-b318-f9633f996684/hls/20200518_214550.m3u8");
               hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
                 console.log("manifest loaded, found " + data.levels.length + " quality level");
               });
